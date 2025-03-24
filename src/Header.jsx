51f489725254fe8096c75d1baf4f5fc58384af71
mihainemes423 <mihainemes423@gmail.com>
2025-03-24 19:39:45 +0200

    header1

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
 export default function Header() {
   return (
-    <header className="header">
-      <h1 className="title">My Website</h1>
-      <nav className="nav">
+    <header className="header-container">
+      <div className="header-logo">
+        <img src="/logo.png" alt="Logo" />
+        <h1 className="header-title">
+          Joé <span className="bold">Wilson</span>
+        </h1>
+      </div>
+
+      <nav className="nav-container">
         <ul className="nav-list">
-          <li><a href="#" className="nav-link">Home</a></li>
-          <li><a href="#" className="nav-link">About</a></li>
-          <li><a href="#" className="nav-link">Services</a></li>
-          <li><a href="#" className="nav-link">Contact</a></li>
+          <li className="nav-item"><a href="#">Home ▼</a></li>
+          <li className="nav-item"><a href="#">About</a></li>
+          <li className="nav-item"><a href="#">Resume</a></li>
+          <li className="nav-item"><a href="#">Portfolio</a></li>
+          <li className="nav-item"><a href="#">Contacts</a></li>
+          <li className="nav-item"><a href="#">Blog</a></li>
         </ul>
       </nav>
-      <button className="button">See more</button>
+
+      <button className="download-button">
+        Download CV <span className="download-icon">⬇</span>
+      </button>
     </header>
   );
 }
